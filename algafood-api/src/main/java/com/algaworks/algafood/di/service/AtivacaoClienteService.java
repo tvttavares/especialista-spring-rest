38e0f00959a51a3645252278d3c000c6728161cc
thiago <tvttavares@gmail.com>
2020-04-20 22:33:48 -0400

    Codigo fonte Capitulo 2

---
+++ algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -0,0 +1,33 @@
+package com.algaworks.algafood.di.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.notificacao.NivelUrgencia;
+import com.algaworks.algafood.di.notificacao.Notificador;
+import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
+
+//@Component
+public class AtivacaoClienteService {
+
+	@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
+	@Autowired
+	private Notificador notificador;
+
+//	@PostConstruct
+	public void init() {
+		System.out.println("INIT " + notificador);
+	}
+
+//	@PreDestroy
+	public void destroy() {
+		System.out.println("DESTROY");
+	}
+
+	public void ativar(Cliente cliente) {
+		cliente.ativar();
+
+		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+	}
+
+}
