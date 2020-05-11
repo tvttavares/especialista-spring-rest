cc5097948ef6df9d6901939833341a73009e9748
thiago <tvttavares@gmail.com>
2020-05-11 09:57:18 -0400

    Implementando consultas JPQL em repositórios

--- 05.01-implementando-consultas-jpql-em-repositorios/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ 05.01-implementando-consultas-jpql-em-repositorios/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -10,13 +10,11 @@ import com.algaworks.algafood.domain.model.Cozinha;
 public interface CozinhaRepository {
 
 	List<Cozinha> listar();
-
 	List<Cozinha> consultarPorNome(String nome);
-
 	Cozinha buscar(Long id);
-
+	
+	
 	Cozinha salvar(Cozinha cozinha);
-
 	void remover(Long id);
-
+	
 }
--- 05.01-implementando-consultas-jpql-em-repositorios/algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ 05.01-implementando-consultas-jpql-em-repositorios/algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -17,38 +17,40 @@ public class CozinhaRepositoryImpl implements CozinhaRepository {
 
 	@PersistenceContext
 	private EntityManager manager;
-
+	
 	@Override
 	public List<Cozinha> listar() {
-		return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
+		return manager.createQuery("from Cozinha", Cozinha.class)
+				.getResultList();
 	}
-
+	
 	@Override
 	public List<Cozinha> consultarPorNome(String nome) {
 		return manager.createQuery("from Cozinha where nome like :nome", Cozinha.class)
-				.setParameter("nome", "%" + nome + "%").getResultList();
+			.setParameter("nome", "%" + nome + "%")
+			.getResultList();
 	}
-
+	
 	@Override
 	public Cozinha buscar(Long id) {
 		return manager.find(Cozinha.class, id);
 	}
-
+	
 	@Transactional
 	@Override
 	public Cozinha salvar(Cozinha cozinha) {
 		return manager.merge(cozinha);
 	}
-
+	
 	@Transactional
 	@Override
 	public void remover(Long id) {
 		Cozinha cozinha = buscar(id);
-
+		
 		if (cozinha == null) {
 			throw new EmptyResultDataAccessException(1);
 		}
-
+		
 		manager.remove(cozinha);
 	}
 
