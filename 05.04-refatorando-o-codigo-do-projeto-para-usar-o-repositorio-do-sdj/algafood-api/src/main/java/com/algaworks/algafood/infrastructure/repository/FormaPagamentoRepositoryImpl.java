5ffcc30d79c04d0c9f8a030a9da43b3366e71051
thiago <tvttavares@gmail.com>
2020-05-11 10:22:18 -0400

    Desafio: Refatorando todos os repositórios para usar SDJ

--- 05.04-refatorando-o-codigo-do-projeto-para-usar-o-repositorio-do-sdj/algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/FormaPagamentoRepositoryImpl.java
+++ 05.04-refatorando-o-codigo-do-projeto-para-usar-o-repositorio-do-sdj/algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/FormaPagamentoRepositoryImpl.java
@@ -5,34 +5,35 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.FormaPagamento;
 import com.algaworks.algafood.domain.repository.FormaPagamentoRepository;
 
-@Repository
+@Component
 public class FormaPagamentoRepositoryImpl implements FormaPagamentoRepository {
 
 	@PersistenceContext
 	private EntityManager manager;
-
+	
 	@Override
 	public List<FormaPagamento> listar() {
-		return manager.createQuery("from FormaPagamento", FormaPagamento.class).getResultList();
+		return manager.createQuery("from FormaPagamento", FormaPagamento.class)
+				.getResultList();
 	}
-
+	
 	@Override
 	public FormaPagamento buscar(Long id) {
 		return manager.find(FormaPagamento.class, id);
 	}
-
+	
 	@Transactional
 	@Override
 	public FormaPagamento salvar(FormaPagamento formaPagamento) {
 		return manager.merge(formaPagamento);
 	}
-
+	
 	@Transactional
 	@Override
 	public void remover(FormaPagamento formaPagamento) {
