80a77cea1f26a044b99fd0f2ea1421fddc2eed36
thiago <tvttavares@gmail.com>
2020-05-14 09:47:17 -0400

    Implementando um repositório SDJ customizado

--- 05.11-implementando-um-repositorio-sdj-customizado/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ 05.11-implementando-um-repositorio-sdj-customizado/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -11,19 +11,20 @@ import org.springframework.stereotype.Repository;
 import com.algaworks.algafood.domain.model.Restaurante;
 
 @Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries {
+public interface RestauranteRepository 
+		extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries {
 
 	List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
-
+	
 //	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
 	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
-
+	
 //	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
-
+	
 	Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
-
+	
 	List<Restaurante> findTop2ByNomeContaining(String nome);
-
+	
 	int countByCozinhaId(Long cozinha);
-
+	
 }
