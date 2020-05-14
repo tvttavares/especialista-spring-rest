dc3f9e740734633e2743087b1c426ed82b123810
thiago <tvttavares@gmail.com>
2020-05-14 10:44:18 -0400

    Implementando Specifications com SDJ

--- 05.17-implementando-specifications-com-sdj/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ 05.17-implementando-specifications-com-sdj/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -12,20 +12,21 @@ import org.springframework.stereotype.Repository;
 import com.algaworks.algafood.domain.model.Restaurante;
 
 @Repository
-public interface RestauranteRepository
-		extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries, JpaSpecificationExecutor<Restaurante> {
+public interface RestauranteRepository 
+		extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries,
+		JpaSpecificationExecutor<Restaurante> {
 
 	List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
-
+	
 //	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
 	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
-
+	
 //	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
-
+	
 	Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
-
+	
 	List<Restaurante> findTop2ByNomeContaining(String nome);
-
+	
 	int countByCozinhaId(Long cozinha);
-
+	
 }
