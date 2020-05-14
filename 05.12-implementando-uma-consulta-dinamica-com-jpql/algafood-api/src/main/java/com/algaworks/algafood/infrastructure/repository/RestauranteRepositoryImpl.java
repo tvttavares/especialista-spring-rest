78e2ad40e7129a71445fa7e5f7d12232dc527546
thiago <tvttavares@gmail.com>
2020-05-14 09:56:03 -0400

    Implementando uma consulta dinâmica com JPQL

--- 05.12-implementando-uma-consulta-dinamica-com-jpql/algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ 05.12-implementando-uma-consulta-dinamica-com-jpql/algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -19,35 +19,37 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
 	@PersistenceContext
 	private EntityManager manager;
-
+	
 	@Override
-	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
-
-		StringBuilder jpql = new StringBuilder();
+	public List<Restaurante> find(String nome, 
+			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+		
+		var jpql = new StringBuilder();
 		jpql.append("from Restaurante where 0 = 0 ");
-
-		HashMap<String, Object> parametros = new HashMap<String, Object>();
-
+		
+		var parametros = new HashMap<String, Object>();
+		
 		if (StringUtils.hasLength(nome)) {
 			jpql.append("and nome like :nome ");
 			parametros.put("nome", "%" + nome + "%");
 		}
-
+		
 		if (taxaFreteInicial != null) {
 			jpql.append("and taxaFrete >= :taxaInicial ");
 			parametros.put("taxaInicial", taxaFreteInicial);
 		}
-
+		
 		if (taxaFreteFinal != null) {
 			jpql.append("and taxaFrete <= :taxaFinal ");
 			parametros.put("taxaFinal", taxaFreteFinal);
 		}
-
-		TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
-
+		
+		TypedQuery<Restaurante> query = manager
+				.createQuery(jpql.toString(), Restaurante.class);
+		
 		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
 
 		return query.getResultList();
 	}
-
+	
 }
