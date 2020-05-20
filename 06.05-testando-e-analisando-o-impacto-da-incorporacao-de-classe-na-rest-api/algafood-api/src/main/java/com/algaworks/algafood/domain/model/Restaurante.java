849a9d5fe6b2e3ed98006116f38a6eaddab8f608
Thiago <tvttavares@gmail.com>
2020-05-20 10:44:34 -0400

    Mapeando propriedades com @CreationTimestamp e @UpdateTimestamp

--- 06.05-testando-e-analisando-o-impacto-da-incorporacao-de-classe-na-rest-api/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ 06.05-testando-e-analisando-o-impacto-da-incorporacao-de-classe-na-rest-api/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -1,7 +1,6 @@
 package com.algaworks.algafood.domain.model;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,9 +14,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -33,34 +29,26 @@ public class Restaurante {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	
 	@Column(nullable = false)
 	private String nome;
-
+	
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
-
+	
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
-
+	
 	@JsonIgnore
 	@Embedded
 	private Endereco endereco;
-
-	@JsonIgnore
-	@CreationTimestamp
-	@Column(nullable = false, columnDefinition = "datetime")
-	private LocalDateTime dataCadastro;
-
-	@JsonIgnore
-	@UpdateTimestamp
-	@Column(nullable = false, columnDefinition = "datetime")
-	private LocalDateTime dataAtualizacao;
-
+	
 	@JsonIgnore
 	@ManyToMany
-	@JoinTable(name = "restaurante_forma_pagamento", joinColumns = @JoinColumn(name = "restaurante_id"), inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+	@JoinTable(name = "restaurante_forma_pagamento",
+			joinColumns = @JoinColumn(name = "restaurante_id"),
+			inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
 	private List<FormaPagamento> formasPagamento = new ArrayList<>();
-
+	
 }
--- 06.06-mapeando-propriedades-com-creationtimestamp-e-updatetimestamp/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ 06.06-mapeando-propriedades-com-creationtimestamp-e-updatetimestamp/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -33,34 +33,36 @@ public class Restaurante {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	
 	@Column(nullable = false)
 	private String nome;
-
+	
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
-
+	
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
-
+	
 	@JsonIgnore
 	@Embedded
 	private Endereco endereco;
-
+	
 	@JsonIgnore
 	@CreationTimestamp
 	@Column(nullable = false, columnDefinition = "datetime")
 	private LocalDateTime dataCadastro;
-
+	
 	@JsonIgnore
 	@UpdateTimestamp
 	@Column(nullable = false, columnDefinition = "datetime")
 	private LocalDateTime dataAtualizacao;
-
+	
 	@JsonIgnore
 	@ManyToMany
-	@JoinTable(name = "restaurante_forma_pagamento", joinColumns = @JoinColumn(name = "restaurante_id"), inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+	@JoinTable(name = "restaurante_forma_pagamento",
+			joinColumns = @JoinColumn(name = "restaurante_id"),
+			inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
 	private List<FormaPagamento> formasPagamento = new ArrayList<>();
-
+	
 }
