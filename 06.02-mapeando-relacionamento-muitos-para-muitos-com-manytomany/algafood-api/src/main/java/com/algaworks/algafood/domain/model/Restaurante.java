9851b9ac53f1158fec613f44b81c6e3b551cfb47
Thiago <tvttavares@gmail.com>
2020-05-20 09:49:16 -0400

    Mapeando classes incorporáveis com @Embedded e @Embeddable

--- 06.02-mapeando-relacionamento-muitos-para-muitos-com-manytomany/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ 06.02-mapeando-relacionamento-muitos-para-muitos-com-manytomany/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,8 +13,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -29,23 +26,21 @@ public class Restaurante {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	
 	@Column(nullable = false)
 	private String nome;
-
+	
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
-
+	
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
-
-	@Embedded
-	private Endereco endereco;
-
-	@JsonIgnore
+	
 	@ManyToMany
-	@JoinTable(name = "restaurante_forma_pagamento", joinColumns = @JoinColumn(name = "restaurante_id"), inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+	@JoinTable(name = "restaurante_forma_pagamento",
+			joinColumns = @JoinColumn(name = "restaurante_id"),
+			inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
 	private List<FormaPagamento> formasPagamento = new ArrayList<>();
-
+	
 }
--- 06.04-mapeando-classes-incorporaveis-com-embeddable-e-embedded/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ 06.04-mapeando-classes-incorporaveis-com-embeddable-e-embedded/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -29,23 +29,25 @@ public class Restaurante {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	
 	@Column(nullable = false)
 	private String nome;
-
+	
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
-
+	
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
-
+	
 	@Embedded
 	private Endereco endereco;
-
+	
 	@JsonIgnore
 	@ManyToMany
-	@JoinTable(name = "restaurante_forma_pagamento", joinColumns = @JoinColumn(name = "restaurante_id"), inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+	@JoinTable(name = "restaurante_forma_pagamento",
+			joinColumns = @JoinColumn(name = "restaurante_id"),
+			inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
 	private List<FormaPagamento> formasPagamento = new ArrayList<>();
-
+	
 }
