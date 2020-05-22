e2b674be676426c55c72ee4bc65ae62a8b93b332
Thiago <tvttavares@gmail.com>
2020-05-22 11:50:35 -0400

    Desafio: mapeando relacionamentos muitos-para-muitos

--- algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -9,7 +9,11 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -25,42 +29,41 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
 	@PersistenceContext
 	private EntityManager manager;
-	
-	@Autowired @Lazy
-	private RestauranteRepository restauranteRepository;
-	
-	@Override
-	public List<Restaurante> find(String nome, 
-			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
-		var builder = manager.getCriteriaBuilder();
-		
-		var criteria = builder.createQuery(Restaurante.class);
-		var root = criteria.from(Restaurante.class);
 
-		var predicates = new ArrayList<Predicate>();
-		
+	@Autowired
+	@Lazy
+	private RestauranteRepository restauranteRepository;
+
+	@Override
+	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+
+		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+		Root<Restaurante> root = criteria.from(Restaurante.class);
+
+		ArrayList<Predicate> predicates = new ArrayList<Predicate>();
+
 		if (StringUtils.hasText(nome)) {
 			predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
 		}
-		
+
 		if (taxaFreteInicial != null) {
 			predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
 		}
-		
+
 		if (taxaFreteFinal != null) {
 			predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
 		}
-		
+
 		criteria.where(predicates.toArray(new Predicate[0]));
-		
-		var query = manager.createQuery(criteria);
+
+		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		return query.getResultList();
 	}
 
 	@Override
 	public List<Restaurante> findComFreteGratis(String nome) {
-		return restauranteRepository.findAll(comFreteGratis()
-				.and(comNomeSemelhante(nome)));
+		return restauranteRepository.findAll(comFreteGratis().and(comNomeSemelhante(nome)));
 	}
-	
+
 }
